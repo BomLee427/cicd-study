f1c4a924d9e4b405aa6904ab591dbb51ff4752a7
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-12-22 20:49:08 +0900

    -

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -3,6 +3,9 @@ IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
 CONTAINER_ENV_PATH="/home/ubuntu/env/.env"
 SERVICE_NAME=cicd
 
+echo aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 491092715218.dkr.ecr.ap-northeast-2.amazonaws.com
+echo docker pull ${IMAGE_NAME}
+
 # Docker Compose YAML을 새로운 도커 버전으로 작성해서 저장
 echo "version: '3.8'
 
@@ -11,15 +14,7 @@ services:
     container_name: cicd-study
     image: ${IMAGE_NAME}
     ports:
-      - 8080:8080
-    env_file:
-      - ${CONTAINER_ENV_PATH}
-    depends_on:
-
-networks:
-  default:
-    name: i-network
-    external: true" > docker-compose.yaml
+      - 8080:8080" > docker-compose.yaml
 
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
