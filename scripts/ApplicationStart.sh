7bc90bb20f18e378ea5cfab18d3b234b2bfd9c9c
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-12-31 22:50:02 +0900

    -

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,15 +1,11 @@
-IMAGE_FILE_PATH="/home/ubuntu/deploy/image.txt"
-IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
+IMAGE_NAME=_IMAGE_NAME_
 SERVICE_NAME=cicd
 
 sudo aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 491092715218.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker image prune -f
 sudo docker pull ${IMAGE_NAME}
 
-echo "왜없을까요..."
-cd /home/ubuntu/deploy
-ls
-
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
+cd /home/ubuntu/deploy
 sudo docker compose up -d $SERVICE_NAME
